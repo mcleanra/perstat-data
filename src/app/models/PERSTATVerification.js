329d91353cf6482cb8d585c458feca8e14ea398b
Ryan McLean <mcleanra@gmail.com>
2017-04-20 12:01:15 +0200

    added model for PERSTATVerifications

---
+++ src/app/models/PERSTATVerification.js
@@ -0,0 +1,38 @@
+(function (angular) {
+
+    'use strict';
+
+    angular.module('perstat.data')
+        .factory('PERSTATVerification', ['spListItem', function (spListItem) {
+
+            var _siteUrl = _spPageContextInfo.siteAbsoluteUrl;
+            var _listName = "PERSTATVerifications";
+            var _viewFields =
+                "<ViewFields>\
+                    <FieldRef Name='ID' />\
+                    <FieldRef Name='Title' />\
+                    <FieldRef Name='Day' />\
+                    <FieldRef Name='Verified' />\
+                </ViewFields>";
+
+            var _spServicesJsonMapping = {
+                ows_ID: { mappedName: "Id", objectType: "Number" },
+                ows_Title: { mappedName: "Title", objectType: "Text" },
+                ows_Day: { mappedName: "Day", objectType: "DateTime" },
+                ows_Verified: { mappedName: "Verified", objectType: "Boolean" }
+            };
+
+            var PERSTATVerification = function (item) {
+                this.Id = item.Id;
+                this.Title = item.Title;
+                this.Day = item.Day;
+                this.Verified = item.Verified;
+            };
+
+            PERSTATVerification.prototype = new spListItem(_siteUrl, _listName, _viewFields, _spServicesJsonMapping);
+
+            return PERSTATVerification;
+
+        }]);
+
+})(angular);
---
+++ src/app/services/perstatVerificationsService.js
@@ -0,0 +1,15 @@
+﻿(function (angular) {
+
+	'use strict';
+
+	angular.module('perstat.data')
+		.factory("perstatVerificationsService", ['PERSTATVerification', 'spListService',
+			function (PERSTATVerification, spListService) {
+
+				var svc = new spListService(PERSTATVerification);
+
+				return svc;
+
+			}]);
+			
+})(angular);
