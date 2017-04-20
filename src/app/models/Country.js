fec9760430b6d3cd7b7f36ec93cdbe94b9c561b5
Ryan McLean <mcleanra@gmail.com>
2017-04-20 12:11:47 +0200

    added model for Tbl_Countries

---
+++ src/app/models/Country.js
@@ -0,0 +1,44 @@
+(function (angular) {
+
+    'use strict';
+
+    angular.module('oaa.data')
+        .factory('Country', ['spListItem', function (spListItem) {
+
+            var _siteUrl = _spPageContextInfo.siteAbsoluteUrl;
+            var _listName = "Tbl_Countries";
+            var _viewFields =
+                "<ViewFields>\
+                    <FieldRef Name='ID' />\
+                    <FieldRef Name='Title' />\
+                    <FieldRef Name='Capital' />\
+                    <FieldRef Name='lat' />\
+                    <FieldRef Name='long' />\
+                    <FieldRef Name='CI_x0020_Threat_x0020_Level' />\
+                </ViewFields>";
+
+            var _spServicesJsonMapping = {
+                ows_ID: { mappedName: "Id", objectType: "Number" },
+                ows_Title: { mappedName: "Title", objectType: "Text" },
+                ows_Capital: {mappedName: "Capital", objectType: "Text"},
+                ows_lat: {mappedName: "lat", objectType: "Number"},
+                ows_long: {mappedName: "long", objectType: "Number"},
+                ows_CI_x0020_Threat_x0020_Level: { mappedName: "CI_x0020_Threat_x0020_Level", objectType: "Choice"}
+            };
+
+            var Country = function (item) {
+                this.Id = item.Id;
+                this.Title = item.Title;
+                this.Capital = item.Capital;
+                this.lat = item.lat;
+                this.long = item.long;
+                this.CI_x0020_Threat_x0020_Level = item.CI_x0020_Threat_x0020_Level;
+            };
+
+            Country.prototype = new spListItem(_siteUrl, _listName, _viewFields, _spServicesJsonMapping);
+
+            return Country;
+
+        }]);
+
+})(angular);
---
+++ src/app/services/countryService.js
@@ -0,0 +1,13 @@
+﻿(function (angular) {
+
+	'use strict';
+
+	angular.module('oaa.data').factory("countryService", ['Country', 'spListService', function (Country, spListService) {
+
+		var svc = new spListService(Country);
+
+		return svc;
+
+	}]);
+
+})(angular);
