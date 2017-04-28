2380bf0928b974486ef6fa7bafff7b15ca7961b8
mcleanra <mcleanra@gmail.com>
2017-04-28 16:26:34 +0200

    v3.0.2

--- dist/perstat-data.js
+++ dist/perstat-data.js
@@ -3,20 +3,8 @@
     'use strict';
 
   var app = angular.module('perstat.data', [
-		'angular.sp.list',
-    'angular.sp.digest'
-	])
-  .config(function(){
-    
-    window._spFormDigestRefreshInterval = 1440000;
-
-  })
-  .run(['RequestDigestIntervalService', function(RequestDigestIntervalService){
-
-    //this refreshes the request digest every 24 minutes, allowing us to post info to SharePoint
-    RequestDigestIntervalService.startInterval('/apps');
-    
-  }]);
+		'angular.sp.list'
+	]);
 
   app.value('_', window._);
   app.value('moment', window.moment);
